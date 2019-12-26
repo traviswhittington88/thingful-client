0da60d66acb8f20c52db87dd76e63203aa7645c5
traviswhittington88 <traviswhittington@ymail.com>
2019-12-26 17:06:54 -0600

    added service: AuthApiService

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Button, Input } from '../Utils/Utils'
+import AuthApiService from '../../services/auth-api-service'
 import TokenService from '../../services/token-service'
 
 export default class LoginForm extends Component {
@@ -20,6 +21,26 @@ export default class LoginForm extends Component {
     user_name.value = ''
     password.value = ''
     this.props.onLoginSuccess()
+  }
+
+  handleSubmitJwtAuth = ev => {
+    ev.preventDefault()
+    this.setState({ error: null })
+    const { user_name, password } = ev.target
+
+    AuthApiService.postLogin({
+      user_name: user_name.value,
+      password: password.value
+    })
+      .then(res => {
+        user_name.value = ''
+        password.value = ''
+        TokenService.saveAuthToken(res.authToken)
+        this.props.onLoginSuccess()
+      })
+      .catch(res => {
+        this.setState({ error: res.error })
+      })
   }
 
   render() {
